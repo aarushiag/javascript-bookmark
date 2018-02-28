4dab7a138906f071c1e2f1729842f66b67232e61
Aarushi Agarwal <aarushi16216@iiitd.ac.in>
2018-02-28 21:30:17 +0530

    Add files via upload

--- bookmark/js/main.js
+++ bookmark/js/main.js
@@ -4,6 +4,9 @@ documents.getElementById('myform').addEventListener('submit',savingBookmark);
 
 function savingBookmark(e)
 {
+	var siteName=document.getElementById('siteName').value;
+	var siteUrl=document.getElementById('siteUrl').value;
+
 	console.log('It is Working');
 	e.preventDefault();
 }
